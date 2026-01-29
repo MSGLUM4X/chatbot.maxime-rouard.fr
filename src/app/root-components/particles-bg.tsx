3e2fc54e26953d9a3a97c02b63b154618d662e2c
Maxime Rouard <rouard.maxi@gmail.com>
2026-01-29 16:14:20 +0100

    prototype Home

---
+++ src/app/root-components/particles-bg.tsx
@@ -0,0 +1,61 @@
+import styles from "@/ui/styles/particles.module.css";
+
+const ParticlesBg = () => {
+    return (
+        <>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+            <div className={styles.particle}></div>
+        </>
+    )
+}
+
+
+export default  ParticlesBg;
---
+++ src/app/root-components/service-button.tsx
@@ -0,0 +1,16 @@
+const ServiceButton = () => {
+
+    return (
+        <div
+            className={`h-[70px] cursor-pointer shadow-2xl rounded-2xl flex flex-row items-center justify-start group transition-all duration-300 ease-out hover:-translate-y-1 hover:scale-105 hover:shadow-xl`}
+        >
+            <div className='flex-1 flex items-center justify-center'>
+                <div className={`transition-all duration-300  font-bold font-mono text-lg`}>
+                    In coming
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default ServiceButton;
